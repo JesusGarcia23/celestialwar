36e3ae23cfeb316c6baf05c6e9125001dafb8c10
= <jacex17@gmail.com>
2020-08-21 21:30:28 -0700

    Add warrior pedestal file and basic constructor

---
+++ src/components/WarriorPedestal/index.js
@@ -0,0 +1,14 @@
+class WarriorPedestal {
+    constructor(height, width, x, y){
+        this.height = height;
+        this.width = width;
+        this.x = x;
+        this.y = y;
+        this.activated = false;
+        this.side = null;
+    }
+
+    drawPedestal = () => {
+
+    }
+}
