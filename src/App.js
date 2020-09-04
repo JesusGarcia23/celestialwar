83c3751b8b90f3c3e1e80065fa9a612d40f79425
= <jacex17@gmail.com>
2020-09-03 18:56:48 -0700

    Add Lobby page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import logo from './logo.svg';
 import './App.css';
 import Battlefield from './components/Battlefield';
 import LandingPage from './pages/LandingPage';
+import Lobby from './pages/Lobby';
 
 function App() {
   return (
     <div className="App">
       <Switch>
       <Route exact path='/' component={LandingPage}/>
+      <Route exact path='/lobby' component={Lobby}/>
       <Route exact path='/test' component={Battlefield}/>
       </Switch>
     </div>
---
+++ src/pages/Lobby/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Lobby = (props) => {
+    return (
+        <div>
+            Lobby
+        </div>
+    )
+}
+
+export default Lobby;
