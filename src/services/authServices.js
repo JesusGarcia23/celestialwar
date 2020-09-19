4c1d74f879c2ab97629fb2086651340682cbfb4a
= <jacex17@gmail.com>
2020-09-18 21:53:11 -0700

    Add axios

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,3 +1,19 @@
-export const isUserLoggedIn = () => {
+import api from './config';
+
+export const isUserLoggedIn = async () => {
     const user = localStorage.getItem('token') || localStorage.getItem('token')
+
+    return await api.get('/isUserLoggedIn', 
+    {
+        params: {
+            user: user
+          }
+    })
+    .then(response => {
+        if(response.status === 200) {
+            console.log(response);
+        }
+    }).catch(err => {
+        return err.response;
+    })
 }
