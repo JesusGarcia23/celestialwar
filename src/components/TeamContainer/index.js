8b4bf361cfe254da2ce908c55b7e7402bb6e16c8
= <jacex17@gmail.com>
2020-09-20 12:16:29 -0700

    Add team player individual container

--- src/components/TeamContainer/index.js
+++ src/components/TeamContainer/index.js
@@ -4,7 +4,39 @@ import './style.css';
 const TeamContainer = () => {
     return (
         <div className="team-container">
-        Teams Container
+            <div className="angel-team-container">
+                <div className="angel-team-socket">
+                </div>
+
+                <div className="angel-team-socket">
+                </div>
+
+                <div className="angel-team-socket">
+                </div>
+
+                <div className="angel-team-socket">
+                </div>
+
+                <div className="angel-team-socket">
+                </div>
+            </div>
+
+            <div className="demon-team-container">
+                <div className="demon-team-socket">
+                </div>
+
+                <div className="demon-team-socket">
+                </div>
+
+                <div className="demon-team-socket">
+                </div>
+
+                <div className="demon-team-socket">
+                </div>
+
+                <div className="demon-team-socket">
+                </div>
+            </div>
         </div>
     )
 }
