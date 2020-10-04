ab28bcc8e23ed3af4bb56de1d666ae258ff2c450
= <jacex17@gmail.com>
2020-10-04 14:29:53 -0700

    Add pre-battle component

---
+++ src/components/PreBattle/index.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './style.css';
+
+const PreBattle = (props) => {
+
+    const { angelTeam, demonTeam } = props;
+
+    return (
+        <div className="pre-battle-container">
+            <h2>Pre Battle</h2>
+            <div className="">
+            </div>
+
+            <div>
+            </div>
+        </div>
+    )
+}
+
+export default PreBattle;
--- src/pages/Room/index.js
+++ src/pages/Room/index.js
@@ -4,11 +4,14 @@ import TeamContainer from '../../components/TeamContainer';
 import Context from '../../Context/Context';
 import { joinRoom, leaveRoom } from '../../sockets/emit/roomEmit';
 import { Redirect } from 'react-router-dom';
+import PreBattle from '../../components/PreBattle';
 import './style.css';
 
 const Room = (props) => {
 
     const MyContext = useContext(Context);
+
+    const [ preBattle, setPreBattle ] = useState(false);
 
     const { id } = props.match.params;
 
@@ -25,6 +28,10 @@ const Room = (props) => {
         props.history.push('/lobby');
     }
 
+    const prepareGame = () => {
+        setPreBattle(true);
+    }
+
     if (error.kicked && (error.kicked.value && error.kicked.room.toString() === id)) {
 
         return <Redirect to ='/lobby'/>
@@ -32,25 +39,31 @@ const Room = (props) => {
     } else {
 
     return (
+        
         <div className="room-container">
-            <div>
-            {user.username === actualRoom.host && <h1>You're host</h1>}
-            <button onClick={e => goBackToLobby(e)}>Exit room</button>
+            {!preBattle ?
+                <>
+                    <div className="room-header">
+                        {user.username === actualRoom.host && <h2>You're host</h2>}
+                        <button onClick={e => goBackToLobby(e)}>Exit room</button>
 
-            <div className="settings-container">
-                <div>Map: {actualRoom && actualRoom.settings && actualRoom.settings.map}</div>
-                <div></div>
-                <div></div>
-                <div></div>
-                {user.username === actualRoom.host && <button>Change Settings</button>}
-            </div>
+                        <div className="settings-container">
+                            <div>Map: {actualRoom && actualRoom.settings && actualRoom.settings.map}</div>
+                            <div>Players: {actualRoom && actualRoom.players && actualRoom.players.length}/10</div>
+                            <div></div>
+                            <div></div>
+                            {user.username === actualRoom.host && <button>Change Settings</button>}
+                        </div>
 
-            </div>
-            <div className="room-team-chat-container">
-                <TeamContainer angelTeam={angelTeam} demonTeam={demonTeam} user={user} actualRoom={actualRoom}/>
-                <Chat messagesList={actualRoom.messages} user={user} roomId={actualRoom.id}/>
-            </div>
-            {user.username === actualRoom.host && <button>Start Game</button>}
+                    </div>
+                    <div className="room-team-chat-container">
+                        <TeamContainer angelTeam={angelTeam} demonTeam={demonTeam} user={user} actualRoom={actualRoom}/>
+                        <Chat messagesList={actualRoom.messages} user={user} roomId={actualRoom.id}/>
+                    </div>
+                    {user.username === actualRoom.host && <button onClick={() => prepareGame()}>Start Game</button>}
+                </>
+                : <PreBattle angelTeam={angelTeam} demonTeam={demonTeam}/>
+            }
         </div>
     )
             
