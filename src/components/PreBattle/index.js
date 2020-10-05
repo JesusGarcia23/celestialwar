6e9c318d356d8e1778eda3f0aa78ee12a0a532e5
= <jacex17@gmail.com>
2020-10-04 17:55:39 -0700

    Add basic layout to pre battle screen

--- src/components/PreBattle/index.js
+++ src/components/PreBattle/index.js
@@ -5,13 +5,36 @@ const PreBattle = (props) => {
 
     const { angelTeam, demonTeam } = props;
 
+    const [ myTeam, setMyTeam ] = useState([]);
+
     return (
         <div className="pre-battle-container">
             <h2>Pre Battle</h2>
-            <div className="">
-            </div>
-
-            <div>
+            <div className="pre-battle-teams-container">
+                <div className="pre-battle-team">
+                    <div className="pre-battle-team-socket">
+                    </div>
+                    <div className="pre-battle-team-socket">
+                    </div>
+                    <div className="pre-battle-team-socket-king">
+                    </div>
+                    <div className="pre-battle-team-socket">
+                    </div>
+                    <div className="pre-battle-team-socket">
+                    </div>
+                </div>
+                <div className="pre-battle-team">
+                    <div className="pre-battle-team-socket">
+                    </div>
+                    <div className="pre-battle-team-socket">
+                    </div>
+                    <div className="pre-battle-team-socket-king">
+                    </div>
+                    <div className="pre-battle-team-socket">
+                    </div>
+                    <div className="pre-battle-team-socket">
+                    </div>
+                </div>
             </div>
         </div>
     )
